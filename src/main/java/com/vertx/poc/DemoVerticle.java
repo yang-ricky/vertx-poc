f148ec040150dce805f70a7a3dd7587eacf8c2c0
Ricky Yang <ricky.yang@ef.com>
2022-05-04 23:27:13 +0800

    add route

--- src/main/java/com/vertx/poc/DemoVerticle.java
+++ src/main/java/com/vertx/poc/DemoVerticle.java
@@ -1,18 +1,18 @@
 package com.vertx.poc;
 
-
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
+import io.vertx.ext.web.RoutingContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import io.vertx.ext.web.Router;
 import static org.springframework.beans.factory.config.BeanDefinition.SCOPE_PROTOTYPE;
 
 @Component
-
 @Scope(SCOPE_PROTOTYPE)
 public class DemoVerticle extends AbstractVerticle {
     private static final Logger LOG = LoggerFactory.getLogger(DemoVerticle.class);
@@ -22,9 +22,11 @@ public class DemoVerticle extends AbstractVerticle {
 
     @Override
     public void start(Promise<Void> startPromise) {
-        vertx.createHttpServer().requestHandler(request -> {
-            request.response().end(greeter.healthCheck());
-        }).listen(8080, ar -> {
+        Router router = Router.router(vertx);
+        router.route("/").handler(this::getDefault);
+        router.route("/health").handler(this::getHealthCheck);
+
+        vertx.createHttpServer().requestHandler(router).listen(8080, ar -> {
             if (ar.succeeded()) {
                 LOG.info("GreetingVerticle started: @" + this.hashCode());
                 startPromise.complete();
@@ -32,5 +34,13 @@ public class DemoVerticle extends AbstractVerticle {
                 startPromise.fail(ar.cause());
             }
         });
+    }
+
+    private void getHealthCheck(RoutingContext ctx) {
+        ctx.response().end("HealthCheck");
+    }
+
+    private void getDefault(RoutingContext ctx) {
+        ctx.response().end("default");
     }
 }
